419d752df4e9402232abd11781d316932808fdc0
Luong Minh Nhat <luongminhnhat1604@gmail.com>
2025-06-24 11:06:33 +0700

    remove help on plugins

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ function App() {
               plugins: [
                 'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
                 'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+                'insertdatetime', 'media', 'table', 'code', 'wordcount'
               ],
               toolbar: 'undo redo | blocks | ' +
                 'bold italic forecolor | alignleft aligncenter ' +
